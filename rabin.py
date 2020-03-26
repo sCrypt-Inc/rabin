43855139cadf85dcea8b865bd4e047252e0043b5
Xiaohui <whulxh@gmail.com>
2020-03-26 12:46:00 -0700

    convert to python3

--- rabin.py
+++ rabin.py
@@ -1,6 +1,6 @@
 import random, hashlib, sys
 
-nrabin = 19357582850646234094870243936597881526816985728422822188584707758040190772124753795991976921183046753452348096483832662365979754187531420465198820567318721654423603010043133474548809492401416756882260459015012465339112151702054910778727561990594846430780059397082161240247533519862291928917720568887309L
+nrabin = 1116705369667683094806847219508144970451437099933488450284517797141150201629195047119398549706064070681325529749675621392780646572135106869032095184212513
 
 
 def bin2num(x):
@@ -37,7 +37,7 @@ def num2hextxt(x):
   h__ = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
   while x > 0:
     res = h__[x % 16] + res
-    x /= 16
+    x //= 16
   return res
 
 def gcd(a,b):
@@ -69,7 +69,7 @@ def nextPrime_3(p):
 # x: bytes
 # return: int
 def h(x):
-  return int.from_bytes(hashlib.sha256(x), "big")
+  return int(hashlib.sha256(x).hexdigest(), 16)
   # dx1 = hashlib.sha512(x).digest()
   # dx2 = hashlib.sha512(dx1+x).digest()
   # dx3 = hashlib.sha512(x+dx2).digest()
@@ -85,32 +85,22 @@ def root(m, p, q):
   i = 0
   while True:
     x = h(m) % nrabin
-    sig =   pow(p,q-2,q) * p * pow(x,(q+1)/4,q) 
-    sig = ( pow(q,p-2,p) * q * pow(x,(p+1)/4,p) + sig ) % (nrabin) 
+    sig =   pow(p,q-2,q) * p * pow(x,(q+1)//4,q) 
+    sig = ( pow(q,p-2,p) * q * pow(x,(p+1)//4,p) + sig ) % (nrabin) 
     if (sig * sig) % nrabin == x:
       break
     m = m + bytes.fromhex("00")
     i = i + 1
-  print "paddingnum: " + str(i)
+  print("paddingnum: " + str(i))
   return sig
 
 def writeNumber(number, fnam):
-  f = open(fnam, 'wb')
-  n = number
-  while n > 0:
-    byte = n % 256
-    n = n / 256
-    f.write(chr(byte))
-  f.close()
+  with open(fnam + '.txt', 'w') as f:
+    f.write('%d' % number)
 
 def readNumber(fnam):
-  f = open(fnam, 'rb')
-  n = 0
-  snum = f.read()
-  for i in range(len(snum)):
-    n = (n << 8) ^ ord(snum[len(snum)-i-1])   
-  f.close()
-  return n
+  with open(fnam + '.txt', 'r') as f:
+    return int(f.read())
 
 def random512():
   md = hashlib.sha512("RANDOM-SEED")
@@ -132,33 +122,34 @@ def hF(m, paddingnum):
 def sF(hexmsg):
   p = readNumber("p")
   q = readNumber("q")
-  return root(hexmsg, p, q)
+  return root(bytes.fromhex(hexmsg), p, q)
+
 
 def vF(hexmsg, paddingnum, s):
   return hF(bytes.fromhex(hexmsg), paddingnum) == (s * s) % nrabin
  
-print "\n\n rabin signature - copyright Scheerer Software 2018 - all rights reserved\n\n"
-print "First parameter is V (Verify) or S (Sign)\n\n"
-print "\n\n verify signature (2 parameters):"
-print "   > python rabin.py V <hexmessage> <paddingnum> <digital signature> "
+print("\n\n rabin signature - copyright Scheerer Software 2018 - all rights reserved\n\n")
+print("First parameter is V (Verify) or S (Sign)\n\n")
+print("\n\n verify signature (2 parameters):")
+print("   > python rabin.py V <hexmessage> <paddingnum> <digital signature> ")
 
-print " create signature S (2 parameter):"
-print "   > python rabin.py S <hexmessage> \n\n"
+print(" create signature S (2 parameter):")
+print("   > python rabin.py S <hexmessage> \n\n")
 
-print " number of parameters is " + str(len(sys.argv)-1)
-print " "
-print " "
+print(" number of parameters is " + str(len(sys.argv)-1))
+print(" ")
+print(" ")
 
 if len(sys.argv) == 5 and sys.argv[1] == "V":
-  print "result of verification: " + str(vF(sys.argv[2], int(sys.argv[3]), hextxt2num(sys.argv[4])))
+  print("result of verification: " + str(vF(sys.argv[2], int(sys.argv[3]), hextxt2num(sys.argv[4]))))
 
 if len(sys.argv) == 3 and sys.argv[1] == "S":
-  print(" digital signature:\n " + num2hextxt(sF(sys.argv[2])))
+  print((" digital signature:\n " + num2hextxt(sF(sys.argv[2]))))
      
 if len(sys.argv) == 3 and sys.argv[1] == "G":
-  print " generate primes ... "
+  print(" generate primes ... ")
   p = nextPrime( h(bytes.fromhex(sys.argv[2])) % (2**501 + 1) )  
   q = nextPrime( h(bytes.fromhex(sys.argv[2] + '00')) % (2**501 + 1) )  
   writeNumber(p, 'p')                     
   writeNumber(q, 'q')     
-  print "nrabin = ", (p * q)
+  print("nrabin = ", (p * q))
