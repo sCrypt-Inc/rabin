17ddea37e8a590617c389f57b024f6707bfcbb60
Xiaohui <whulxh@gmail.com>
2020-03-26 17:06:04 -0700

    refactor: remove dead code

--- rabin.py
+++ rabin.py
@@ -1,44 +1,6 @@
-import random, hashlib, sys
+import hashlib, sys
 
-nrabin = 1116705369667683094806847219508144970451437099933488450284517797141150201629195047119398549706064070681325529749675621392780646572135106869032095184212513
-
-
-def bin2num(x):
-  res = 0
-  for c in x:
-    res = (res<<8) ^ ord(c)
-  return res
-
-def num2bin(x):
-  res = ''
-  while x > 0:
-    res = chr(x % 256) + res
-    x /= 256
-  return res
-
-def digital2num(x):
-  res = 0
-  for c in x:
-    if ord(c) >= 48 and ord(c) <= 57:
-      res = (res*10) + ord(c) - 48
-  return res
-
-def hextxt2num(x):
-  res = 0
-  for c in x:
-    if ord(c) < 58 and ord(c) >= 48:
-       res = (res<<4) + ord(c) - 48
-    elif ord(c) <= ord('f') and ord(c) >= ord('a'):
-       res = (res<<4) + ord(c) - 87
-  return res
-
-def num2hextxt(x):
-  res = ''
-  h__ = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
-  while x > 0:
-    res = h__[x % 16] + res
-    x //= 16
-  return res
+nrabin = 0x15525796ddab817a3c54c4bea4ef564f090c5909b36818c1c13b9e674cf524aa3387a408f9b63c0d88d11a76471f9f2c3f29c47a637aa60bf5e120d1f5a65221
 
 def gcd(a,b):
   if b > a:
@@ -70,15 +32,6 @@ def nextPrime_3(p):
 # return: int
 def h(x):
   return int(hashlib.sha256(x).hexdigest(), 16)
-  # dx1 = hashlib.sha512(x).digest()
-  # dx2 = hashlib.sha512(dx1+x).digest()
-  # dx3 = hashlib.sha512(x+dx2).digest()
-  # dx4 = hashlib.sha512(x+dx3).digest()
-  # dx5 = hashlib.sha512(x+dx4).digest()
-  # res = 0
-  # for cx in (dx1+dx2+dx3+dx4+dx5):
-  #   res = (res<<8) ^ ord(cx)
-  # return res
 
 # m: bytes
 def root(m, p, q):
@@ -101,20 +54,6 @@ def writeNumber(number, fnam):
 def readNumber(fnam):
   with open(fnam + '.txt', 'r') as f:
     return int(f.read())
-
-def random512():
-  md = hashlib.sha512("RANDOM-SEED")
-  md.update('large key value for generation of random number')
-  md.update( str(random.random()) )
-  md.update( str(random.random()) )
-  result = 0
-  largestr = md.digest()
-  for i in range(len(largestr)):
-      result = (result << 8) ^ ord(largestr[i])
-  return result
-
-def random1024():
-  return random512() * random512()
 
 def hF(m, paddingnum):
   return h(m + bytes.fromhex("00") * paddingnum) % nrabin
@@ -141,10 +80,10 @@ print(" ")
 print(" ")
 
 if len(sys.argv) == 5 and sys.argv[1] == "V":
-  print("result of verification: " + str(vF(sys.argv[2], int(sys.argv[3]), hextxt2num(sys.argv[4]))))
+  print("result of verification: " + str(vF(sys.argv[2], int(sys.argv[3]), int(sys.argv[4], 16))))
 
 if len(sys.argv) == 3 and sys.argv[1] == "S":
-  print((" digital signature:\n " + num2hextxt(sF(sys.argv[2]))))
+  print((" digital signature:\n " + hex(sF(sys.argv[2]))))
      
 if len(sys.argv) == 3 and sys.argv[1] == "G":
   print(" generate primes ... ")
@@ -152,4 +91,4 @@ if len(sys.argv) == 3 and sys.argv[1] == "G":
   q = nextPrime( h(bytes.fromhex(sys.argv[2] + '00')) % (2**501 + 1) )  
   writeNumber(p, 'p')                     
   writeNumber(q, 'q')     
-  print("nrabin = ", (p * q))
+  print("nrabin = ", hex(p * q))
